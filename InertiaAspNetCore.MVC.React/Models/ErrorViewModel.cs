bff7a0f74bbf8271cf3805988e73484084fc9c65
Tanguy <Tanguy@DESKTOP-NS3GRTP>
2024-07-22 18:11:47 +0200

    Adding examples folder and project

---
+++ InertiaAspNetCore.MVC.React/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InertiaAspNetCore.MVC.React.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
